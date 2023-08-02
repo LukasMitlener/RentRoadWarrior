c6b2936ae8420cfa11346f2e31227e3bc9e0dde0
Lukáš Mitlener <97617930+LukasMitlener@users.noreply.github.com>
2023-08-02 12:45:23 +0200

    Add files via upload

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -32,7 +32,7 @@ const CarCard = ({ car }: CarCardProps) => {
         <span className='self-end text-[14px] leading-[17px] font-medium'>/day</span>
       </p>
 
-      <div className='relative w-full h-40 my-3 object-contain'>
+      <div className='relative w-full h-48 my-3 object-contain'>
         <Image src={imagePath} alt='car model' fill priority className='object-contain' />
       </div>
 
--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -53,7 +53,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => (
                 </button>
 
                 <div className='flex-1 flex flex-col gap-3'>
-                  <div className='relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg'>
+                  <div className='relative w-full h-80 bg-pattern bg-cover bg-center rounded-lg'>
                     <Image src={car.imagePath} alt='car model' layout="fill" priority className='object-contain' />
                   </div>
 
